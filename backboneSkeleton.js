fb6ca3b95b3877b2df3c7289f2950f991244de52
Mikeronomicon <mikeronomicon@gmail.com>
2015-06-30 09:24:24 -0700

    new skeleton, 3min 22sec

---
+++ backboneSkeleton.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+	var thisModel = Backbone.Model.extend ({
+
+	})
+
+	var thisView = Backbone.View.extend ({
+
+	})
+
+	var thisCollection = Backbone.Collection.extend ({
+
+	})
+});
